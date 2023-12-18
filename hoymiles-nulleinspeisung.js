ed2f82d0665125e1e6b9cdaf345fa3056a6128f1
Schmakus <8231359+Schmakus@users.noreply.github.com>
2023-12-18 15:13:38 +0100

    Update hoymiles-nulleinspeisung.js

--- hoymiles-nulleinspeisung.js
+++ hoymiles-nulleinspeisung.js
@@ -1,70 +1,69 @@
-//ALPHA VERSION!!!!
-
 const inverters = [
 	{
   	name: "Inverter 1",
     maxPower: 1600,
     newLimit: 100,
     oldLimit: 100,
-    grid: 1200,
+    grid: 500,
   },
   {
   	name: "Inverter 2",
     maxPower: 1600,
     newLimit: 100,
     oldLimit: 100,
-    grid: 1500,
+    grid: 500,
   },
   {
   	name: "Inverter 3",
     maxPower: 1600,
     newLimit: 100,
     oldLimit: 100,
-    grid: 633,
+    grid: 833,
   },
 ]
 
-const powerPhaseA = -300;
+const powerPhaseA = -400;
 const powerPhaseB = 0;
 const powerPhaseC = 0;
 
 const limitMin = 5;
 const limitMax = 100;
 
-async function reload() {
+async function check() {
 	const powerSum = powerPhaseA + powerPhaseB + powerPhaseC;
 	const gridSum = await gridSumme();
   const neededPower = await gridSum + powerSum;
   const maxInverterSumme = await maxInverterSum();
   const limitSum = await percent(neededPower, maxInverterSumme, 100);
   console.log(`limitSum: ${limitSum} %, neededPower: ${neededPower} W, gridSum: ${gridSum} W, maxInverterPower: ${maxInverterSumme}`);
-
-  let newInverterPowerSum = 0;
-  let limitInverterSum = 0;
   
-  if (Math.abs(gridSum - neededPower) <= 100) {
-    	console.log(`${wr.name}: Nichts zu tun. Erzeugte Leistung liegt im Bereich der gemessen Leistung`);
-      //return;
+  if (powerSum >= -300 && powerSum <= 0  || (neededPower - gridSum) < 100) {
+    	console.log(`Nichts zu tun. Erzeugte Leistung liegt im Bereich der gemessen Leistung`);
+      await reload();
+      return;
   }
-  
-  console.log(`Begrenzung durchführen...`);
-  
-  for (let i = 0; i < inverters.length; i++) {
-  	wr = inverters[i];
-
-    const factor = await percent(wr.grid, gridSum, 100);    
-    const newlimitPower = await round(neededPower * factor / 100);
-    const newLimitPercent = await percentAndLimit(limitPower, wr.maxPower, 100);
-        
-    console.log(`${wr.name}: Factor = ${factor}, newLimitPercent = ${newLimitPercent} %, new maximumPower = ${newlimitPower} W`);
     
-    await setLimit(wr, newLimitPercent);
-  
-  } 
+  for (let i = 0; i < inverters.length; i++) {    
+  	const inverter = inverters[i];
+    
+    const factor = await percent(inverter.grid, gridSum, 100);    
+    const newlimitPower = await round(neededPower * factor / 100);
+    const newLimitPercent = await percentAndLimit(newlimitPower, inverter.maxPower, 100);
+        
+    //console.log(`${inverter.name}: Factor = ${factor}, newLimitPercent = ${newLimitPercent} %, new maximumPower = ${newlimitPower} W`);
+    
+    await setLimit(inverter, newLimitPercent);  
+  }  
+  await reload();
 }
 
 async function setLimit(inverter, limit) {
-  console.log(`Neues Limit für ${wr.name} gesetzt.`)
+  if (inverter.oldLimit === limit) {
+    console.log(`Limit von ${inverter.name} ist gleich altem Limit. Keine Begrenzung`);
+    return;
+  }
+  inverter.oldLimit = limit;
+  console.log(`Neues Limit ${limit} % für ${inverter.name} gesetzt.`);
 }
 
 async function gridSumme() {
@@ -104,6 +103,9 @@ async function percentAndLimit(power1, power2, factor) {
   return result;
 }
 
-reload();
+async function reload() {
+  await new Promise(resolve => setTimeout(resolve, 5000));
+  await check();
+}
 
-
+check();
